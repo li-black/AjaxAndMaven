b108999c311369087f279cbffcb7086b63f2eb31
li-black <1436988790@qq.com>
2021-03-07 19:27:38 +0800

    ajax first comm

--- AjaxAndMaven/day01_ajax_2/src/com/itheima/controller/NewsServlet2.java
+++ AjaxAndMaven/day01_ajax_2/src/com/itheima/controller/NewsServlet2.java
@@ -1,8 +1,10 @@
 package com.itheima.controller;
-//瀑布流分页查询控制层
+//按钮分页查询控制层
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.pagehelper.Page;
+import com.github.pagehelper.PageInfo;
+import com.itheima.bean.User;
 import com.itheima.service.NewsService;
 import com.itheima.service.impl.NewsServiceImpl;
 
@@ -12,9 +14,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
-@WebServlet("/newsServlet")
-public class NewsServlet extends HttpServlet {
+@WebServlet("/newsServlet2")
+public class NewsServlet2 extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         //设置请求和响应的编码
@@ -23,12 +26,15 @@ public class NewsServlet extends HttpServlet {
         //1.获取请求参数
         String start = req.getParameter("start");
         String pageSize = req.getParameter("pageSize");
-        //2.根据当前页码和每页显示的条数，调用业务层的方法，得到分页Page对象
+        //2.根据当前页码和每页显示的条数来调用业务层的查询方法，得到分页Page对象
         NewsService service = new NewsServiceImpl();
         Page page = service.pageQuery(Integer.parseInt(start), Integer.parseInt(pageSize));
-        //3.将得到的数据转为json
-        String json = new ObjectMapper().writeValueAsString(page);
-        //4.将数据响应给客户端
+        //3.封装PageInfo对象
+        PageInfo<List<User>> pageInfo = new PageInfo<>(page);
+        //4.将得到的数据转为JSON
+        String json = new ObjectMapper().writeValueAsString(pageInfo);
+        System.out.println(json);
+        //5.将数据响应给客户端
         resp.getWriter().write(json);
     }
 
